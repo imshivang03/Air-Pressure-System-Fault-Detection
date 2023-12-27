d6681642c6c22246f14c947984fc02308c5b07e8
imshivang03 <shivang.cse.nitnagaland@gmail.com>
2023-12-27 18:29:30 +0530

    Updated the code

--- sensor/config.py
+++ sensor/config.py
@@ -7,6 +7,7 @@ import os
 #Provide the mongodb localhost url to connect python to mongodb.
 @dataclass
 class EnvironmentVariable:
-    mongo_db_url: os.getenv("MONGO_DB_URL")
+    mongo_db_url: str= os.getenv("MONGO_DB_URL")
 
-client= pymongo.MongoClient()
+env_var= EnvironmentVariable()
+mongo_client= pymongo.MongoClient(env_var.mongo_db_url)
